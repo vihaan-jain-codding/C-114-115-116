6ebdaf3935b3626ce7b7eb0528bed796d8659546
vihaan-jain-codding <94980425+vihaan-jain-codding@users.noreply.github.com>
2023-11-04 15:27:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX = 0;
+noseY = 0;
+function preload(){
+clownImage = loadImage("https://i.postimg.cc/HsTrCvKX/clownnose.png");
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotposes);
+}
+
+function modelloaded(){
+    console.log("PoseNet is initialized.");
+}
+
+function gotposes(results){
+    if (results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        console.log("Nose X = " + noseX);
+        noseY = results[0].pose.nose.y-15;
+        console.log("Nose Y = " + noseY);
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clownImage, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('filter.png');
+}
